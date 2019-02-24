40f1e1d19ebebac168c31776032b6fe67fc7767b
François Trahan <ftrahan@enerosolutions.com>
2019-02-23 19:54:11 -0500

    test_noparams

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -7,15 +7,16 @@ from exstats.app import run
 
 
 class ExitException(Exception):
-    def __init__(self, code) -> None:
+    def __init__(self, code, message) -> None:
+        self.message = message
         self.code = code
 
 
 
 @fixture
 def exitmock():
-    def raiseOnExit(code):
-        raise ExitException(code)
+    def raiseOnExit(self, code=0, message=None):
+        raise ExitException(code, message)
 
 
     with patch("argparse.ArgumentParser.exit", raiseOnExit) as patcher:
@@ -23,18 +24,33 @@ def exitmock():
 
 
 
+def test_noparams(exitmock, capsys):
+    EXPECTED_ERR = "usage: exstat [-h] SRC [SRC ...]\n"
+    EXPECTED_OUT = ""
+
+    with raises(ExitException) as ex:
+        run([])
+
+        assert ex.value.code == 0
+
+    out, err = capsys.readouterr()
+
+    assert EXPECTED_OUT == out
+    assert EXPECTED_ERR == err
+
+
+
 def test_help(exitmock, capsys):
     EXPECTED_ERR = ""
-    EXPECTED_OUT = """usage: exstat [-h] SRC [SRC ...]
-
-Computes disk usage statistics by file extension
-
-positional arguments:
-  SRC         A file or a folder to be included
-
-optional arguments:
-  -h, --help  show this help message and exit
-"""
+    EXPECTED_OUT = ("usage: exstat [-h] SRC [SRC ...]\n"
+                    "\n"
+                    "Computes disk usage statistics by file extension\n"
+                    "\n"
+                    "positional arguments:\n"
+                    "  SRC         A file or a folder to be included\n"
+                    "\n"
+                    "optional arguments:\n"
+                    "  -h, --help  show this help message and exit\n")
 
     with raises(ExitException) as ex:
         run("-h".split())
